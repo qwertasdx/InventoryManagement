a8236a3b6282ef638238d70282763402223ebd46
qwertasdx <a103025030@gmail.com>
2024-12-06 18:04:18 +0800

    feat():調整Html，調整成DisplayNameFor。調整管理使用者、盤點紀錄查詢、庫存量查詢，將管理人員字體為紅色、盤虧調紅色、安全庫存量為0調整成紅色

--- Dto/ItemStocks.cs
+++ Dto/ItemStocks.cs
@@ -1,19 +1,26 @@
-﻿namespace InventoryManagement.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManagement.Dto
 {
     public class ItemStocks
     {
+        [Display(Name = "商品名稱")]
         public string ItemName { get; set; } = null!;
 
+        [Display(Name = "商品貨號")]
         public string ItemCode { get; set; } = null!;
 
+        [Display(Name = "單位")]
         public string Unit { get; set; } = null!;
 
         public int ?SafeQty { get; set; }
 
+        [Display(Name = "總數量")]
         public int TotalQty { get; set; }
 
-        public string Status { get; set; } = null!;
+        public string Status { get; set; } = null!; 
 
+        [Display(Name = "狀態")]
         public string StatusName { get; set; } = null!;
 
         public string EmployeeId { get; set; } = null!;
--- Dto/Users.cs
+++ Dto/Users.cs
@@ -1,15 +1,22 @@
-﻿namespace InventoryManagement.Dto
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManagement.Dto
 {
     public class Users
     {
+        [Display(Name = "員工編號")]
         public string EmployeeId { get; set; } = null!;
 
+        [Display(Name = "員工姓名")]
         public string EmployeeName { get; set; } = null!;
 
+        [Display(Name = "帳號")]
         public string Account { get; set; } = null!;
 
         public string Role { get; set; } = null!;
 
+        [Display(Name = "身分")]
         public string RoleName { get; set; } = null!;
     }
 }
